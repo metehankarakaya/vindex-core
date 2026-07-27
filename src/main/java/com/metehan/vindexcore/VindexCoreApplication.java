bd6a25007aaaae8fe417cdf9a884952f77423101
Metehan KARAKAYA <karakayametehan00@gmail.com>
2026-07-28 02:19:58 +0300

    Initial commit

---
+++ src/main/java/com/metehan/vindexcore/VindexCoreApplication.java
@@ -0,0 +1,13 @@
+package com.metehan.vindexcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VindexCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VindexCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metehan/vindexcore/VindexCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metehan.vindexcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VindexCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
